7871323d8c5490ec4a0823eeccc78d4b20b54887
chanhyun <acexr@naver.com>
2015-10-07 02:14:21 +0900

    onlybomb수정
    폭탄충돌체크

--- onlybomb.py
+++ onlybomb.py
@@ -12,36 +12,43 @@ boomri=load_image('boomright.bmp')
 boomle=load_image('boomleft.bmp')
 boomdo=load_image('boomdown.bmp')
 boomup=load_image('boomup.bmp')
-def mathsqrt(cx,cy,blx,b1y):
-    return math.sqrt(((cx)-blx) *((cx)-blx) + ((cy)-bly)*((cy)-bly))
+def mathsqrt(bx,by,bx2,by2):
+    return math.sqrt((bx2-bx) *(bx2-bx) + (by2-by)*(by2-by))
 x=0
 y=0
 bx=0
 by=0
 bx2=0
 by2=0
-SIZE=256
+
 count=0
 frame1=0
 boomcenterframe=0
 boomframe=0#폭탄터질때 프레
 timer=0
+BSIZE=20
 boomcount=1#0되면 폭탄터진 프레임도 사라짐
-bombcount=1#0되면 폭탄 사라짐
+bombcount=1#1은 폭탄이 생기게 하는 변수
 bcy=0
 while 1:
+  
+  
+  clear_canvas()
   timer+=1
+  bx=200
+  by=100
+  bx2=250
+  by2=100
   boomframe=(boomframe+1)%8
   boomcenterframe=(boomcenterframe+1)%3
-  clear_canvas()
+  bomb.clip_draw(frame1*45,0,47,bcy,bx2,by2)
   if(bombcount==1 and timer==1):
     bcy=50#폭탄 화면에서 없애기 메모리는계속있음..
   #bx=random.randrange(1,10,2)
   #by=random.randrange(0,8,2)
-  bx=200
-  by=100
+ 
   
-  #boomup.clip_draw(boomframe*40,0,40,50,bx,by+50)
+  
   bomb.clip_draw(frame1*45,0,47,bcy,bx,by)
   frame1=(frame1+1)%4
   
@@ -50,21 +57,25 @@ while 1:
     
     timer=0
     boom.clip_draw(boomcenterframe*50,0,40,50,bx,by)#center는 프레임 세개임
-    boomup.clip_draw(boomframe*40,0,40,50,bx,by+50)#나머지 상하좌우는 8개임
-    boomdo.clip_draw(boomframe*40,0,40,50,bx,by-50)
-    boomle.clip_draw(boomframe*40,0,40,50,bx-40,by)
-    boomri.clip_draw(boomframe*40,0,40,50,bx+40,by)
+    boomup.clip_draw(boomframe*40,0,40,50,bx,by+50)#나머지 상하좌우는 8개임 count=1
+    boomdo.clip_draw(boomframe*40,0,40,50,bx,by-50)#count=2
+    boomle.clip_draw(boomframe*40,0,40,50,bx-40,by)#count=3
+    boomri.clip_draw(boomframe*40,0,40,50,bx+40,by)#count=4
     bombcount=1
     boomcenterframe=(boomcenterframe+1)%3
     boomframe=(boomframe+1)%8
     timer=0
+
+    if(mathsqrt(bx+40,by,bx2-10,by2)<BSIZE):
+      boom.clip_draw(boomcenterframe*50,0,40,50,bx2,by2)
+      
   update_canvas()
   
   get_events()
-  delay(0.3)  
+  delay(0.1)  
   
 
-
+  
 
 update_canvas()
    
